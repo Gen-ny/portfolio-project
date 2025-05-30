df8f6bb3ad1b3d9f443828c694f9265a83906cf6
Genevieve <genevieveappia2@gmail.com>
2025-05-30 14:06:42 +0000

    first commit

---
+++ my-portfolio/index.js
@@ -0,0 +1,36 @@
+function opentab(event, tabname) {
+    const tablinks = document.getElementsByClassName("tab-links");
+    const tabcontents = document.getElementsByClassName("tab-contents");
+
+    // Deactivate all tabs and hide all content
+    for (let tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (let tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+
+    // Activate the clicked tab and show the corresponding content
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  const text = "I am Genevieve Appiah from Ghana";
+  const typewriter = document.getElementById("typewriter");
+  let index = 0;
+
+  function type() {
+    if (index < text.length) {
+      typewriter.textContent += text.charAt(index);
+      index++;
+      setTimeout(type, 100); // typing speed
+    }
+  }
+
+  type();
+});
